773b204aa8781c04f5f9d04fb74f4ff0fac73663
bdbrink <brendan.brink@outlook.com>
2023-07-10 17:48:40 -0500

    adding comments

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
+# Load the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-split_classes = [8,9]
 
+# Specify the classes to be filtered out
+split_classes = [8, 9]
+
+# Create boolean masks to filter out specific classes from the training set
 mask_train_8 = np.isin(y_train, split_classes, invert=True).flatten()
+
+# Create boolean masks to filter out specific classes from the test set
 mask_test_8 = np.isin(y_test, split_classes, invert=True).flatten()
 
+# Print the first 20 elements of the training set mask
 print(mask_train_8[:20])
